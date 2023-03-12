081a0bf0d70dfb93af4ec48093d480b77e54e080
ma5hti <127637516+ma5hti@users.noreply.github.com>
2023-03-12 03:58:20 +0000

    Added graphics and menu except play option

--- c4game.py
+++ c4game.py
@@ -1,3 +1,5 @@
+import time
+
 # --- Graphic Board ---
 # prints the board in cosole with all the current values in lists r1-r6
 
@@ -56,6 +58,127 @@ def vertical_check(letter):
 def player_check(letter):
     return diagonal_check(letter) or horizontal_check(letter) or vertical_check(letter)
 
+# --- Main Menu ---
+# this function prints the mainmenu and takes inputs and opens new sections accordingly
+
+def main_menu_input():
+    print(
+"""
+░█▄█░█▀▀░█▀█░█░█
+░█░█░█▀▀░█░█░█░█
+░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+P - Play
+S - Scores & Players
+H - Help
+C - Credits
+E - Exit
+"""
+    )
+    tmp = ""
+    while tmp.upper() not in game_menu:
+        tmp = input()
+
+    tmp = tmp.upper()
+    if tmp == "P":
+        pass
+    elif tmp == "S":
+        print(
+"""
+░█▀▀░█▀▀░█▀█░█▀▄░█▀▀░█▀▀
+░▀▀█░█░░░█░█░█▀▄░█▀▀░▀▀█
+░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀▀
+
+"""
+        )
+        print(player_x_name + " (X): " + str(player_x_score))
+        print(player_o_name + " (O): " + str(player_o_score))
+        print("\n--------------------\n\n")
+        print(
+"""
+░█▀█░█░░░█▀█░█░█░█▀▀░█▀▄░█▀▀
+░█▀▀░█░░░█▀█░░█░░█▀▀░█▀▄░▀▀█
+░▀░░░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀
+
+"""
+        )
+        print("Player X: " + player_x_name)
+        print("Player O: " + player_o_name)
+        print("\n--------------------\n\n")
+        print("C - Change Names\nM - Main Menu")
+        tmp2 = ""
+        while tmp2.upper() != "C" and tmp2.upper() != "M":
+            tmp2 = input()
+        if tmp2.upper() == "C":
+            get_names()
+        else:
+            main_menu_input()
+    elif tmp == "H":
+        print(
+        """
+░█░█░█▀▀░█░░░█▀█
+░█▀█░█▀▀░█░░░█▀▀
+░▀░▀░▀▀▀░▀▀▀░▀░░
+
+A two-player connection game called "Connect Four"
+has the players choose either "X" or "O" before taking
+turns placing tokens into a seven-column, six-row rack.
+The pieces land straight down, taking up the lowest
+space in the column. Becoming the first player to
+arrange four of their own tokens in a line that is
+either horizontal, vertical, or diagonal is the game's
+goal.
+
+----------------------------
+
+M - Main Menu
+        """
+        )
+        back_input()
+    elif tmp == "C":
+        print(
+"""
+░█▀▀░█▀▄░█▀▀░█▀▄░▀█▀░▀█▀░█▀▀
+░█░░░█▀▄░█▀▀░█░█░░█░░░█░░▀▀█
+░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░░▀░░▀▀▀
+
+Mohammadamin Moezzi
+(portfolio project)
+2023
+Toronto, Canada
+
+----------------------------
+
+M - Main Menu
+        """
+        )
+        back_input()
+    else:
+        print("GOODBYE!")
+        exit()
+
+# --- Back Input ---
+# this function is for going back in certain menu's
+
+def back_input():
+    tmp = ""
+    while tmp.upper() != "M":
+        tmp = input()
+    main_menu_input()
+
+# --- Player Name Input ---
+# This functions takes and saves the players' names in global variables
+
+def get_names():
+    global player_x_name
+    global player_o_name
+    player_x_name = input("What is the name for player X? ")
+    player_o_name = input("What is the name for player O? ")
+    print("Player X: " + player_x_name)
+    print("Player O: " + player_o_name)
+    time.sleep(3)
+    main_menu_input()
+
 # --- INITIAL VARIABLES ---
 # 6 lists with 7 empty spots as in the original 6x7 board
 
@@ -68,3 +191,38 @@ r6 = [" ", " ", " ", " ", " ", " ", " "]
 board = [r1, r2, r3, r4, r5, r6]
 letters = ["A", "B", "C", "D", "E", "F", "G"]
 numbers = ["1", "2", "3", "4", "5", "6"]
+
+player_x_score = 0
+player_o_score = 0
+
+game_menu = ["P", "S", "H", "C", "E"]
+
+# --- Game Greeting ---
+print(
+"""
+ _ _ _ _____ __    _____ _____ _____ _____ 
+| | | |   __|  |  |     |     |     |   __|
+| | | |   __|  |__|   --|  |  | | | |   __|
+|_____|_____|_____|_____|_____|_|_|_|_____|
+"""
+)
+print(
+"""
+ _____ _____ 
+|_   _|     |
+  | | |  |  |
+  |_| |_____|
+"""
+)
+print(
+"""
+ _____ _____ _____ _____ _____ _____ _____    ___ 
+|     |     |   | |   | |   __|     |_   _|  | | |
+|   --|  |  | | | | | | |   __|   --| | |    |_  |
+|_____|_____|_|___|_|___|_____|_____| |_|      |_|
+"""
+)
+print("___________________________________________\n\n")
+time.sleep(3)
+print("--- Please tell us your names ---")
+get_names()
