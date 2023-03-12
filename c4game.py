eaaf9670abc930919963bbe381bc50c4764cd961
ma5hti <127637516+ma5hti@users.noreply.github.com>
2023-03-12 02:32:41 +0000

    Added diagonal check logic.

--- c4game.py
+++ c4game.py
@@ -15,7 +15,22 @@ def print_board():
         print(str(i+1) + "| " + tmp)
 
     print("    - - - - - - - \n    A B C D E F G ")
-        
+
+# --- Diagonal Check ---
+# this function checks if there are any diagonal sets of 4 for input "X" or "O"
+
+def diagonal_check(letter):
+    for i in range(3):
+        for j in range(4):
+            if board[i][j] == letter and board[i+1][j+1] == letter and board[i+2][j+2] == letter and board[i+3][j+3] == letter:
+                return True
+    for i in range(3):
+        for j in range(-1, -5, -1):
+            if board[i][j] == letter and board[i+1][j-1] == letter and board[i+2][j-2] == letter and board[i+3][j-3] == letter:
+                return True
+
+    return False
+
 
 # --- INITIAL VARIABLES ---
 # 6 lists with 7 empty spots as in the original 6x7 board
